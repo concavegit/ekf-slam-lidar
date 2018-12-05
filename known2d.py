68da2f4699becb7a525062d061ad65e953cf7d5c
Kawin Nikomborirak <concavemail@gmail.com>
2018-12-05 18:21:27 +0000

    Fix prediction and update order

--- known2d.py
+++ known2d.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 
 
 # Calculate Ground Truth
-dt = 0.1
-ts = np.arange(0, 2 * np.pi, dt)
-xs = np.cos(ts)
-ys = np.sin(ts)
+dt = 0.03
+ts = np.arange(0, 1.5 * np.pi, dt)
+xs = 2 * (1 - np.cos(ts)) * np.cos(ts)
+ys = 2 * (1 - np.cos(ts)) + np.sin(ts)
 
 points = np.stack([xs, ys])
 
@@ -26,27 +26,24 @@ noisyA = measureA + np.random.normal(mean, noise, measureA.shape)
 
 
 def kalman(x, P, measurement, R, F, H, Q):
-    # Prior mean
-    # Calculate residual
-    y = measurement - np.dot(H, x)
+    # Predict
+    xHat = np.dot(F, x)
+    PHat = np.dot(np.dot(F, P), F.T) + Q
 
-    # System uncertainty
+    # Update
+    y = measurement - np.dot(H, xHat)
     S = np.dot(np.dot(H, P), H.T) + R
+    K = np.dot(np.dot(PHat, H.T), np.linalg.inv(S))
 
-    # Kalman gain
-    K = np.dot(np.dot(P, H.T), np.linalg.inv(S))
+    xNew = x + np.dot(K, y)
+    PNew = np.dot(np.eye(K.shape[0]) - np.dot(K, H), PHat)
 
-    # Prediction
-    xHat = x + np.dot(K, y)
-    PNew = np.dot(np.eye(K.shape[0]) - np.dot(K, H), P)
-    PHat = np.dot(np.dot(F, PNew), F.T) + Q
-
-    return xHat, PHat
+    return xNew, PNew
 
 
 def kalmanSim():
     x = np.array([1, 0, 0, 0])
-    P = np.diag([0.1, 0.1, .01, .01])
+    P = np.diag([0.1, 0.1, .1, .1])
     R = np.eye(2) * noise
     F = np.array(
         [[1, 0, dt, 0],
@@ -58,7 +55,7 @@ def kalmanSim():
         [[1, 0, 0, 0],
          [0, 1, 0, 0]])
 
-    Q = np.eye(P.shape[0]) / 100
+    Q = np.eye(P.shape[0]) / 512
 
     result = []
     for measurement in noisyA.T:
@@ -70,6 +67,7 @@ def kalmanSim():
 
 simRes = kalmanSim()
 ax = plt.gca()
+ax.set_aspect('equal')
 ax.plot(xs, ys)
 ax.scatter(noisyA[0], noisyA[1])
 ax.plot(simRes[0], simRes[1])
